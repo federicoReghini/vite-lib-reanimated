8d8d06567f84590b590aef36d7b97cd8e11799dc
federicoReghini <federicoreghini@gmail.com>
2024-01-08 15:57:46 +0100

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import { extname, relative, resolve } from "path";
+import { fileURLToPath } from "node:url";
+import { glob } from "glob";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), dts({ include: ["lib"] })],
+  assetsInclude: ["**/*.json"],
+  build: {
+    copyPublicDir: false,
+    rollupOptions: {
+      external: [
+        "react",
+        "react-native",
+        "react-native-web",
+        "react-native-reanimated",
+        "react-native-svg",
+        "react/jsx-runtime",
+        "react-native-gesture-handler",
+        "react-native-safe-area-context",
+      ],
+      input: Object.fromEntries(
+        glob.sync("lib/**/*.{ts,tsx}").map((file) => [
+          // The name of the entry point
+          relative("lib", file.slice(0, file.length - extname(file).length)),
+          // The absolute path to the entry file
+          fileURLToPath(new URL(file, import.meta.url)),
+        ]),
+      ),
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "[name].js",
+      },
+    },
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      formats: ["es"],
+    },
+  },
+});
